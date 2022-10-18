945a145789839d549492ced12c30e15badbfedfa
andy <a@von-burg.net>
2022-10-18 10:51:18 +0200

    clean up

--- src/app/n2n/bind/mapper/Mapper.php
+++ src/app/n2n/bind/mapper/Mapper.php
@@ -2,9 +2,9 @@
 namespace n2n\bind\mapper;
 
 use n2n\util\magic\MagicContext;
-use n2n\validation\err\ValidationMismatchException;
 use n2n\bind\plan\BindableBoundary;
 use n2n\bind\plan\BindContext;
+use n2n\bind\err\BindMismatchException;
 
 interface Mapper  {
 
@@ -14,7 +14,7 @@ interface Mapper  {
 	 * @param MagicContext $magicContext
 	 * @return bool false if a Mapper could not perform a modification of value due to errors of the passed bindables.
 	 * 	The bind process will be aborted in this case.
-	 * @throws ValidationMismatchException
+	 * @throws BindMismatchException
 	 */
 	function map(BindableBoundary $bindableBoundary, BindContext $bindContext, MagicContext $magicContext): bool;
 
