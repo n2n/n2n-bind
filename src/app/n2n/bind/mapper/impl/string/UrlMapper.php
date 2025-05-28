28e462aec005125245f4aec9e4a749f128616ead
Andreas von Burg <a@von-burg.net>
2025-05-28 15:53:45 +0200

    UrlMapper

--- src/app/n2n/bind/mapper/impl/string/UrlMapper.php
+++ src/app/n2n/bind/mapper/impl/string/UrlMapper.php
@@ -9,10 +9,12 @@ use n2n\validation\validator\impl\Validators;
 use n2n\util\type\TypeConstraints;
 use n2n\util\StringUtils;
 use n2n\bind\mapper\MapperUtils;
+use n2n\util\type\ArgUtils;
 
 class UrlMapper extends SingleMapperAdapter {
     public function __construct(private bool $mandatory = false, private ?array $allowedSchemes = null,
 			private bool $schemeRequired = true) {
+		ArgUtils::valArray($this->allowedSchemes, 'string', true);
     }
 
     protected function mapSingle(Bindable $bindable, BindBoundary $bindBoundary, MagicContext $magicContext): bool {
