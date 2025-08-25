15990583056fa4f0da217c1ec3a5f8ac128c0b82
nikolai <nikolai@schmid.guru>
2025-08-25 21:11:25 +0700

    cleanup

--- src/test/n2n/bind/mapper/impl/op/DoIfSingleClosureMapperTest.php
+++ src/test/n2n/bind/mapper/impl/op/DoIfSingleClosureMapperTest.php
@@ -13,6 +13,8 @@ use n2n\bind\build\impl\target\mock\BindTestClass;
 use n2n\bind\mapper\Mapper;
 use n2n\bind\err\BindMismatchException;
 use n2n\bind\err\UnresolvableBindableException;
+use n2n\bind\plan\Bindable;
+use n2n\validation\lang\ValidationMessages;
 
 class DoIfSingleClosureMapperTest extends TestCase {
 
@@ -157,4 +159,158 @@ class DoIfSingleClosureMapperTest extends TestCase {
 		$this->assertSame(['prop' => 'holeradio'], $result->get());
 	}
 
+	/**
+	 * @throws BindTargetException
+	 * @throws UnresolvableBindableException
+	 * @throws BindMismatchException
+	 */
+	function testDoIfBindableClosureAbortMapper() {
+		$result = Bind::attrs(['prop' => 'holeradio'])->prop('prop',
+				Mappers::valueClosure(fn (string $v) => $v . '-1'),
+				Mappers::doIfBindableClosure(function (Bindable $b) {
+					$this->assertTrue($b->doesExist());
+					$this->assertTrue($b->isValid());
+					$this->assertEquals('holeradio-1', $b->getValue());
+					return true;
+				}, abort: true),
+				Mappers::valueClosure(function () {
+					$this->fail('Mapper should not be called.');
+				}))->toArray()->exec();
+
+		$this->assertFalse($result->isValid());
+	}
+
+	/**
+	 * @throws BindTargetException
+	 * @throws UnresolvableBindableException
+	 * @throws BindMismatchException
+	 */
+	function testDoIfBindableClosureConditionFalseMapperContinues() {
+		$result = Bind::attrs(['prop' => 'holeradio'])
+				->prop('prop',
+						Mappers::valueClosure(fn (string $v) => $v . '-1'),
+						Mappers::doIfBindableClosure(function (Bindable $b) {
+							$this->assertEquals('holeradio-1', $b->getValue());
+							return false;
+						}, abort: true),
+						Mappers::valueClosure(fn (string $v) => $v . '-2'))
+				->toArray()->exec();
+
+		$this->assertTrue($result->isValid());
+		$this->assertSame(['prop' => 'holeradio-1-2'], $result->get());
+	}
+
+	/**
+	 * @throws BindTargetException
+	 * @throws UnresolvableBindableException
+	 * @throws BindMismatchException
+	 */
+	function testDoIfBindableClosureSkipNextMapper() {
+		$result = Bind::attrs(['prop' => 'holeradio'])
+				->prop('prop', Mappers::valueClosure(fn (string $v) => $v . '-1'),
+						Mappers::doIfBindableClosure(function (Bindable $b) {
+							$this->assertEquals('holeradio-1', $b->getValue());
+							return true;
+						}, skipNextMappers: true),
+						Mappers::valueClosure(function () {
+							$this->fail('Mapper should be skipped');
+						}))
+				->toArray()->exec();
+
+		$this->assertSame(['prop' => 'holeradio-1'], $result->get());
+	}
+
+	/**
+	 * @throws BindTargetException
+	 * @throws UnresolvableBindableException
+	 * @throws BindMismatchException
+	 */
+	function testDoIfBindableClosureChLogicalMapper() {
+		$result = Bind::attrs(['prop' => 'holeradio', 'prop2' => 'holeradio!'])
+				->prop('prop', Mappers::doIfBindableClosure(fn (Bindable $b) => true, chLogical: true))
+				->logicalProp('prop2', Mappers::doIfBindableClosure(fn (Bindable $b) => true, chLogical: false))
+				->toArray()->exec();
+
+		$this->assertSame(['prop2' => 'holeradio!'], $result->get());
+	}
+
+	/**
+	 * @throws BindTargetException
+	 * @throws UnresolvableBindableException
+	 * @throws BindMismatchException
+	 */
+	function testDoIfInvalidAbortIfInvalid() {
+		$result = Bind::attrs(['prop' => 'holeradio'])
+				->prop('prop',
+						Mappers::bindableClosure(function (Bindable $bindable) {
+							$bindable->addError(ValidationMessages::invalid());
+						}),
+						Mappers::doIfInvalid(abort: true),
+						Mappers::valueClosure(function () {
+							$this->fail('Mapper should not be called.');
+						}))
+				->toArray()->exec();
+
+		$this->assertFalse($result->isValid());
+	}
+
+	/**
+	 * @throws BindTargetException
+	 * @throws UnresolvableBindableException
+	 * @throws BindMismatchException
+	 */
+	function testDoIfInvalidConditionFalseWhenValid() {
+		$result = Bind::attrs(['prop' => 'holeradio'])
+				->prop('prop',
+						Mappers::cleanString(true, 1, 255),
+						Mappers::doIfInvalid(abort: true),
+						Mappers::valueClosure(function (string $v) {
+							return $v . '-ok';
+						}))
+				->toArray()->exec();
+
+		$this->assertTrue($result->isValid());
+		$this->assertSame(['prop' => 'holeradio-ok'], $result->get());
+	}
+
+	/**
+	 * @throws BindTargetException
+	 * @throws UnresolvableBindableException
+	 * @throws BindMismatchException
+	 */
+	function testDoIfInvalidSkipNextMappersOnInvalid() {
+		$result = Bind::attrs(['prop' => 'holeradio'])
+				->prop('prop',
+						Mappers::valueClosure(fn () => null),
+						Mappers::cleanString(true, 1, 255),
+						Mappers::doIfInvalid(skipNextMappers: true),
+						Mappers::valueClosure(function () {
+							$this->fail('Mapper should be skipped');
+						}))
+				->toArray()->exec();
+
+		$this->assertFalse($result->isValid());
+	}
+
+
+	function testDoIfInvalidChLogicalOnInvalid() {
+		$result = Bind::attrs(['prop' => 'holeradio', 'prop2' => 'holeradio!'])
+				->prop('prop',
+						Mappers::cleanString(true, 1, 5),
+						Mappers::doIfInvalid(chLogical: false),
+						Mappers::bindableClosure(function (Bindable $bindable) {
+							$this->assertFalse($bindable->isLogical());
+						}, true, false))
+				->logicalProp('prop2',
+						Mappers::cleanString(true, 1, 5),
+						Mappers::doIfInvalid(chLogical: false)->setDirtySkipped(false),
+						Mappers::bindableClosure(function (Bindable $bindable) {
+							$this->assertFalse($bindable->isLogical());
+						}, true, false))
+				->toArray()->exec();
+
+		$this->assertFalse($result->isValid());
+		$this->assertCount(1, $result->getErrorMap()->getChild('prop')->getMessages());
+		$this->assertCount(1, $result->getErrorMap()->getChild('prop2')->getMessages());
+	}
 }
